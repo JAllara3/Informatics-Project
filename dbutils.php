e64fa8a7263e5274cb3a363507924c8c315485e7
kflood22 <kaitlynn-flood@uiowa.edu>
2017-04-19 14:23:16 -0500

    Add files via upload
    Katie's dbutils.php

--- dbutils.php
+++ dbutils.php
@@ -20,7 +20,7 @@ function queryDB($query, $db) {
   return ($result);
 }
 
-// How many tuples in the result? Syntactic sugar. // how many records we have
+// How many tuples in the result? Syntactic sugar.
 function nTuples($result) {
   return(mysqli_num_rows($result));
 }
@@ -128,17 +128,115 @@ function makeStringSafe($db, $mystring) {
         return $output;
     }
 
-/*function printBoolean($boolean){
-  if($boolean){
-    return 'Yes';
-  } else {
-    return 'No';
+  //
+  // This function returns "Yes" and "No" in lieu of 1 and 0
+  //
+  function printBoolean($boolean) {
+    if ($boolean) {
+      return 'Yes';
+    } else {
+      return 'No';
+    }
   }
-}
+  
+  //
+  // Checkbox generating function returns html/bootstrap checkboxes based on the contents of a specific table and specific variables within that table
+  // specifying values to show users, and values to return
+  //
+  // $db is a handle to the database
+  // $table is the name of the table
+  // $uiVariable is the name of the variable in $table with values the user will see
+  // $indexVariable ist he name of the variable in $table with the values returned by the form
+  // $checkedBoxes in an array with values corresponding to $indexVariable. If a value is present, then that checkbox is checked by default
+  //
+  function generateCheckboxes($db, $table, $uiVariable, $indexVariable, $checkedBoxes = array()) {
+   
+    // set up a query to get the two variables from the table 
+    $query = "SELECT $uiVariable, $indexVariable FROM $table ORDER BY $uiVariable;";
+    
+    // run the query
+    $result = queryDB($query, $db);
+    
+    // put together html for the checkbox
+    $checkboxHTML = "";
+    
+    while($row = nextTuple($result)) {
+      // for each record in the table
+      $checkboxHTML .= "<div class='checkbox'>";
+      $checkboxHTML .= "<label>";
+      
+      // get the values for the ui and index variables
+      $ui = $row["$uiVariable"];
+      $index = $row["$indexVariable"];
+      
+      // see if this checkbox should be checked by default
+      $checked = "";
 
+      // if there are checkboxes to be checked by default
+        
+      // see if $index is one of the values in $checkedBoxes
+      if (in_array($index, $checkedBoxes)) {
+        // we should check this checkbox
+        $checked = "checked";
+      }
+    
+      // this is where my checkbox goes
+      $checkboxHTML .= "<input type='checkbox' value='$index' name='$table-$indexVariable" . "[]' $checked> $ui";
+      
+      $checkboxHTML .= "</label>";
+      $checkboxHTML .= "</div>\n";
+    }
+    
+    // return the html for the checkboxes
+    return $checkboxHTML;
+    
+  }
+  
+  
+  //
+  // Dropdown list generating function returns html/bootstrap dropdown list based on the contents of a specific table and specific variables within that table
+  // specifying values to show users, and values to return
+  //
+  // $db is a handle to the database
+  // $table is the name of the table
+  // $uiVariable is the name of the variable in $table with values the user will see
+  // $indexVariable ist he name of the variable in $table with the values returned by the form
+  // $defaultValue is the default value for the dropdown list
+  //
+  function generateDropdown($db, $table, $uiVariable, $indexVariable, $defaultValue="") {
+   
+    // set up a query to get the two variables from the table 
+    $query = "SELECT $uiVariable, $indexVariable FROM $table ORDER BY $uiVariable;";
+    
+    // run the query
+    $result = queryDB($query, $db);
+    
+    // put together html for the checkbox
+    $dropdownHTML = "<select class='form-control' name='$table-$indexVariable'>\n";
+    
+    while($row = nextTuple($result)) {
+      // get the values for the ui and index variables
+      $ui = $row["$uiVariable"];
+      $index = $row["$indexVariable"];
+      
+      // check if the current value should be the default value
+      if ($index == $defaultValue) {
+        $selected = "selected='selected'";
+      } else {
+        $selected = "";
+      }
 
-
-*/
-
+      // for each record in the table
+      $dropdownHTML .= "<option value='$index' $selected>$ui</option>\n";
+    }
+    
+    // close select tag
+    $dropdownHTML .="</select>";
+    
+    // return the html for the checkboxes
+    return $dropdownHTML;
+    
+  }    
+  
 
 ?>
