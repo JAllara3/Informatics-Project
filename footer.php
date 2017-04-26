37ea6c39b57c0a5257529c3c25c6da6b5752099c
JAllara3 <allara123@gmail.com>
2017-04-26 17:36:02 -0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang=en>
+	<body>
+		<?php
+        echo "<p>\n</p>";
+        echo "<p>Fastshop.com [all rights reserved]\n";
+		echo "<p>Created By: Kaitlynn Flood, Rena Lin, and Justin Allara</p>";
+		?>
+	</body>
+</html>
