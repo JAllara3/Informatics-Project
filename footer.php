a392a0668eef3da434ab32424ede5a4829a6f53f
JAllara3 <allara123@gmail.com>
2017-04-26 21:10:28 -0500

    Add files via upload
    updated with some style to keep the footer always at the bottom

--- footer.php
+++ footer.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
 <html lang=en>
-	<body>
+	<style>
+  .footer {
+    position: absolute;
+    bottom: 0;
+    height: 20px;
+    background: #f0f0f0;
+    width: 100%;
+  }
+</style>
+	<div class="footer">
 		<?php
         echo "<p>\n</p>";
         echo "<p>Fastshop.com [all rights reserved]\n";
 		echo "<p>Created By: Kaitlynn Flood, Rena Lin, and Justin Allara</p>";
 		?>
-	</body>
+	</div>
 </html>
