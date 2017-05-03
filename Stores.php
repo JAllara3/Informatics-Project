2fd57acafb37eb3ecb7e4c093ee400494ee5b6f9
JAllara3 <allara123@gmail.com>
2017-05-03 13:19:56 -0500

    Add files via upload
    good to go

--- Stores.php
+++ Stores.php
@@ -1,193 +1,147 @@
 <?php
     $name = "Add a New Store or Delete a Current one";
     $menuActive = 1;
+	$header1 = "Store Management";
     include_once('header.php');
 ?>
 
 <?php
-	include_once('dbutils.php');
-	include_once('config.php');
-	
-	$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
-	
-	session_start();
-	
-	if(isset($_SESSION['email'])) {
-		echo "Current accoutn: " . $_SESSION['email'] . "";
-		$query = "SELECT id as userid FROM users WHERE email = '".$_SESSION['email'] . "';";
-		$result = queryDB($query, $db);
-		$row = nextTuple($result);
-		$_SESSION['userid'] = $row['userid'];
-		$userid = $_SESSION['userid'];
-		$storeid = $_SESSION['storeid'];
-	}
-?>
-		<div class="row">
-		    <div class="col-xs-12" style="text-align:center">
-		        <h1>Welcome to Fastshop!</h1>
-                <h3>Manage your stores here</h3>
-		    </div>
-		</div>
-
-
-<div class="row">
-    <div class="col-xs-12">
-        
-<!-- set up html table to show contents -->
-<table class="table table-hover" style="background-color:#fff2e6">
-    <!-- headers for table -->
-    <thead>
-        <th>Store name</th>
-        <th>Key features</th>
-		<th>Location</th>
-    </thead>
-
-<?php
-    include_once('config.php');
-    include_once('dbutils.php');
-    
-    $db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
-    
-    session_start();
-    
-    $query = "SELECT * FROM newStores;";
-    
-    $result = queryDB($query, $db);
-    
-    while ($row = nextTuple($result)) {
-            $newStoresid = $row['id'];
-            echo "\n <tr>";
-            echo "<td>" . $row['name']. "</td>";
-            echo "<td>" . $row['description']. "</td>";
-			echo "<td>" . $row['address'] . "</td>";
-			echo "<td><form action='welcome.php?id=$newStoresid' method='post'>";
-				echo "<td><button type='submit' class='btn btn-default' name='newStores'>GO</button></td>";
-                echo "\t<input type='hidden' name='newStoresid' value='" . $row['id'] . "'>\n";
-			echo "</form></td>";
-			echo "<td>";
-		    echo "</tr> \n";
-    }
-?>
-
-</table>
-    </div>
-</div>
-
-
-<?php
+include_once('config.php');
+include_once('dbutils.php');
 
 if (isset($_POST['submit'])) {
-	
 	$name = $_POST['name'];
 	$description = $_POST['description'];
+	$bg = $_POST['bg'];
 	$address = $_POST['address'];
 	
 	$isComplete = true;
 	
 	$errorMessage = "";
 	
+	//check if each of the required variables is in the table
 	if (!isset($name)) {
-		$errorMessage .= "Please name your store. \n";
+		$errorMessage .= "please give a name to your store.\n";
 		$isComplete = false;
 	}
-	if (!isset($description)) {
-		$errorMessage .= "Please describe what this store offers. \n";
-		$isComplete = false;
-	}
-	if (!isset($address)) {
-		$errorMessage .= "Please provide your stores location. \n";
-		$isComplete = false;
-	}
-	if ($isComplete) {
-		$query = "INSERT INTO newStores(name, description, address) VALUES ('$name', '$description', '$address');";
+	
+	if($isComplete) {
+		$query = "INSERT INTO stores(name, description, bg, address) VALUES ('$name', '$description', '$bg', '$address');";
 		$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
+		
+		// run the insert statement
 		$result = queryDB($query, $db);
 		
-		$success = "Successfully created a new store: " . $name;
+		$storesid = mysqli_insert_id($db);
+		$success = "Succesfully created a new store: " . $name;
 		
-		unset($name, $description, $address);
+		unset($name, $description, $bg, $address);
 	}
 }
+
 ?>
 
-<div class= "row">
+<title>Store Management</title>
+
+<div class="row">
+	<div class="col-xs-12" style="text-align:center">
+		<h1>Welcome to Union!</h1>
+		<h3>Manage your stores here</h3>
+	</div>
+</div>
+
+<!--Show successfully creating new store-->
+<div class="row">
 	<div class = "col-xs-12">
 <?php
-	if (isset($success)) {
-		echo '<div class = "alert alert-success" role="alert">';
+	if(isset($success)) {
+		echo '<div class ="alert alert-success" role="alert">';
 		echo ($success);
-		echo'</div>';
+		echo '</div>';
+	} elseif (isset($_GET['successmessage'])) {
+		echo '<div class="alert alert-success" role="alert">';
+		echo ($_GET['successmessage']);
+		echo '</div>';
+	}
+?>
+
+	</div>
+</div>
+
+<!--errors?-->
+<div class="row">
+	<div class = "col-xs-12">
+<?php
+	if (isset($isComplete) && !$isComplete) {
+		echo '<div class="alert alert-danger" role="alert">';
+		echo ($errorMessage);
+		echo '</div>';
 	}
 ?>
 	</div>
 </div>
 
-<div class="row">
-    <div class="col-xs-12">
-<?php
-    if (isset($isComplete) && !$isComplete) {
-        echo '<div class="alert alert-danger" role="alert">';
-        echo ($errorMessage);
-        echo '</div>';
-    }
-?>            
-    </div>
-</div>
 
-<!--html form for new stores-->
+<!--form to create new stores-->
 <div class="row">
 	<div class="col-xs-12">
 
-<form action="Stores.php" method="post">
-	<div class="form-group">
-		<label for="name">Name:</label>
-		<input type="text" class="form-control" name="name" value = "<?php if($name) { echo $name; } ?>"/>"
-	</div>
-	
-	<div class="form-group">
-		<label for="description">Description: </label>
-		<input type="text" class="form-control" name="description" value = "<?php if($description) { echo $description; } ?>"/>"
-	</div>
-	
-	<div class="form-group">
-		<label for="address">Address: </label>
-		<input type="text" class="form-control" name="address" value = "<?php if($address) { echo $address; } ?>"/>"
-	</div>
-	
-	<button type = "submit" class="btn btn-default" name="submit">Create</button>
+<form action="Stores.php" method="post" enctype="multipart/form-data">
+<!--name-->
+<div class = "form-group">
+	<label for="name">Name:</label>
+	<input type="text" class="form-control" name="name" value ="<?php if($name) { echo $name; } ?>"/>
+</div>
+<!--Description-->
+<div class = "form-group">
+	<label for="description">Description:</label>
+	<input type="text" class="form-control" name="name" value ="<?php if($description) { echo $description; } ?>"/>
+</div>
+<!--address-->
+<div class = "form-group">
+	<label for="address">Address:</label>
+	<input type="text" class="form-control" name="address" value ="<?php if($address) { echo $address; } ?>"/>
+</div>
+
+<button type = "submit" class= "btn btn-default" name="submit">Create</button>
 	
 </form>
-
+		
 	</div>
 </div>
 
+<!--Show contents of new store-->
 <div class="row">
-	<div class="col-xs-12">
-		
+    <div class="col-xs-12">
+        
+<!-- set up html table to show contents -->
 <table class="table table-hover">
-	<thead>
-		<th>Name</th>
-		<th>Description</th>
-		<th>Address</th>
-	</thead>
+    <!-- headers for table -->
+    <thead>
+        <th>Store name</th>
+        <th>Key features</th>
+		<th>Location</th>
+    </thead>
 	
 <?php
 	$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
-	$query = 'SELECT newStores.id, newStores.name, newStores.description, newStores.address FROM newStores;';
+	$query = 'SELECT * FROM stores;';
 	$result = queryDB($query, $db);
 	
 	while($row = nextTuple($result)) {
-		echo "\n<tr>";
+		echo "\n <tr>";
 		echo "<td>" . $row['name'] . "</td>";
 		echo "<td>" . $row['description'] . "</td>";
 		echo "<td>" . $row['address'] . "</td>";
-	}
-	
-?>
-	
-	
-</table>
 		
+		//echo "<td><a href = "updateStores.php?id=" . $row['id'] . "'>edit</a></td>";
+		
+		//echo "<td><a href = "deleteStores.php?id=" . $row['id'] . "'>delete</a></td>";
+		
+		echo "</tr> \n";
+	}
+?>
+</table>
 	</div>
 </div>
 
